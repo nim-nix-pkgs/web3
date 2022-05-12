1eae363f553229714f581c0a403a2a7b08fee05c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:47 -0300

    chore: re index web3

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."web3-master".dir   = "master";
+  inputs."web3-master".owner = "nim-nix-pkgs";
+  inputs."web3-master".ref   = "master";
+  inputs."web3-master".repo  = "web3";
+  inputs."web3-master".type  = "github";
+  inputs."web3-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."web3-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
